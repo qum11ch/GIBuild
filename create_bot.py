96aa29ee527de264903463e2b938b84af97a7e3b
qum11ch <qum1ch@mail.ru>
2023-05-22 10:12:07 +0300

    commit

--- create_bot.py
+++ create_bot.py
@@ -1,9 +1,8 @@
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
-import os
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 storage = MemoryStorage()
 
-bot = Bot(token=os.getenv('TOKEN'))
+bot = Bot(token='6194043101:AAENDQGyy9WefgwZQbFU9ivDke1GBWSjNWs')
 dp = Dispatcher(bot, storage=storage)
